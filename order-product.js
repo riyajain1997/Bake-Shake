ed72cce3371bcfc87a78a6ab31aa90ca290ba4cf
riya jain <riya.jain9497@gmail.com>
2019-01-12 13:20:21 +0530

    change lgoin validation and new page added

--- order-product.js
+++ order-product.js
@@ -1,14 +1,14 @@
 var db = new PouchDB('orderProduct');
-var remoteCouch = false;
 window.onload = function () {
     var cakeWeight = document.getElementById('cake-weight');
     var cakeFlavor = document.getElementById('cake-flavor');
     var cakePrice = document.getElementById('cake-price');
     var productImage = document.getElementById('product-image');
+    var productname = document.getElementById('product-name');
 
     var calculatePrice = function () {
         var price = cakeWeight.value * cakeFlavor.value;
-        console.log(cakeFlavor.value);
+        console.log(cakeFlavor.value,cakeWeight.value);
         cakePrice.innerText = price;
     }
     cakeFlavor.onchange = calculatePrice;
@@ -18,6 +18,7 @@ window.onload = function () {
     db.get('selectedProduct').then(function(doc) {
         console.log('got this',doc);
         productImage.src = doc.productImage;
+        productname.innerText = doc.productname;
         return db.remove(doc);
       }).then(function (result) {
         // handle result
